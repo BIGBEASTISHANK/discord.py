7e4fe1ef8a9f3371204ae6b9de258641cdf011ad
BIGBEASTISHANK <77890275+BIGBEASTISHANK@users.noreply.github.com>
2021-08-23 22:41:28 +0530

    Create setting.py

---
+++ setting.py
@@ -0,0 +1,12 @@
+# Import all modules
+import discord
+import time
+from random import *
+from datetime import datetime
+from discord.ext import commands
+
+# Defining variables
+prefix = "'"
+token = "your bot token"
+status = discord.Status.dnd
+activity = discord.Game("with discord servers 😈")
